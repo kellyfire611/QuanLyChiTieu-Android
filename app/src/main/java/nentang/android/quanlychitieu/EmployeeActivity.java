7c8d0582226442c29a2ed7aecf0c73b56a9f9287
kellyfire <kellyfire611@gmail.com>
2019-10-04 04:16:19 +0700

    Bổ sung khung switch Listview/Gridview

--- app/src/main/java/nentang/android/quanlychitieu/EmployeeActivity.java
+++ app/src/main/java/nentang/android/quanlychitieu/EmployeeActivity.java
@@ -1,15 +1,34 @@
 package nentang.android.quanlychitieu;
 
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewStub;
+import android.widget.AdapterView;
+import android.widget.GridView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class EmployeeActivity extends AppCompatActivity {
+    private ViewStub stubGrid;
+    private ViewStub stubList;
+    private ListView listView;
+    private GridView gridView;
+    private ListViewAdapter listViewAdapter;
+    private GridViewAdapter gridViewAdapter;
+    private List<Product> productList;
+    private int currentViewMode = 0;
+
+    static final int VIEW_MODE_LISTVIEW = 0;
+    static final int VIEW_MODE_GRIDVIEW = 1;
 
     List<Employee> employeeList;
     SQLiteDatabase mDatabase;
@@ -18,17 +37,49 @@ public class EmployeeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_employee);
+//
+//        listViewEmployees = (ListView) findViewById(R.id.listViewEmployees);
+        employeeList = new ArrayList<>();
+//
+//        //opening the database
+//        mDatabase = openOrCreateDatabase(MainActivity.DATABASE_NAME, MODE_PRIVATE, null);
+//
+//        //this method will display the employees in the list
+//        showEmployeesFromDatabase();
+
+        // SHOW
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employee);
 
-        listViewEmployees = (ListView) findViewById(R.id.listViewEmployees);
-        employeeList = new ArrayList<>();
+        stubList = (ViewStub) findViewById(R.id.stub_list);
+        stubGrid = (ViewStub) findViewById(R.id.stub_grid);
 
-        //opening the database
+        //Inflate ViewStub before get view
+
+        stubList.inflate();
+        stubGrid.inflate();
+
+        listView = (ListView) findViewById(R.id.mylistview);
+        gridView = (GridView) findViewById(R.id.mygridview);
+
+        //get list of product
+        //getProductList();
+        // opening the database
         mDatabase = openOrCreateDatabase(MainActivity.DATABASE_NAME, MODE_PRIVATE, null);
 
         //this method will display the employees in the list
         showEmployeesFromDatabase();
+
+        //Get current view mode in share reference
+        SharedPreferences sharedPreferences = getSharedPreferences("ViewMode", MODE_PRIVATE);
+        currentViewMode = sharedPreferences.getInt("currentViewMode", VIEW_MODE_LISTVIEW);//Default is view listview
+        //Register item lick
+        listView.setOnItemClickListener(onItemClick);
+        gridView.setOnItemClickListener(onItemClick);
+
+        switchView();
     }
 
     private void showEmployeesFromDatabase() {
@@ -53,10 +104,90 @@ public class EmployeeActivity extends AppCompatActivity {
         cursorEmployees.close();
 
         //creating the adapter object
-        adapter = new EmployeeAdapter(this, R.layout.list_layout_employee, employeeList, mDatabase);
+        //adapter = new EmployeeAdapter(this, R.layout.list_layout_employee, employeeList, mDatabase);
 
         //adding the adapter to listview
-        listViewEmployees.setAdapter(adapter);
+        //listViewEmployees.setAdapter(adapter);
     }
 
+    private void switchView() {
+
+        if(VIEW_MODE_LISTVIEW == currentViewMode) {
+            //Display listview
+            stubList.setVisibility(View.VISIBLE);
+            //Hide gridview
+            stubGrid.setVisibility(View.GONE);
+        } else {
+            //Hide listview
+            stubList.setVisibility(View.GONE);
+            //Display gridview
+            stubGrid.setVisibility(View.VISIBLE);
+        }
+        setAdapters();
+    }
+
+    private void setAdapters() {
+        if(VIEW_MODE_LISTVIEW == currentViewMode) {
+//            listViewAdapter = new ListViewAdapter(this, R.layout.list_item, productList);
+            listViewAdapter = new ListViewAdapter(this, R.layout.list_item, employeeList, mDatabase);
+            listView.setAdapter(listViewAdapter);
+        } else {
+            //gridViewAdapter = new GridViewAdapter(this, R.layout.grid_item, productList);
+            gridViewAdapter = new GridViewAdapter(this, R.layout.grid_item, employeeList, mDatabase);
+            gridView.setAdapter(gridViewAdapter);
+        }
+    }
+//
+//    public List<Product> getProductList() {
+//        //pseudo code to get product, replace your code to get real product here
+//        productList = new ArrayList<>();
+//        productList.add(new Product(R.drawable.ic_launcher_background, "Title 1", "This is description 1"));
+//        productList.add(new Product(R.drawable.ic_launcher_background, "Title 2", "This is description 2"));
+//        productList.add(new Product(R.drawable.ic_launcher_background, "Title 3", "This is description 3"));
+//        productList.add(new Product(R.drawable.ic_launcher_background, "Title 4", "This is description 4"));
+//        productList.add(new Product(R.drawable.ic_launcher_background, "Title 5", "This is description 5"));
+//        productList.add(new Product(R.drawable.ic_launcher_background, "Title 6", "This is description 6"));
+//        productList.add(new Product(R.drawable.ic_launcher_background, "Title 7", "This is description 7"));
+//        productList.add(new Product(R.drawable.ic_launcher_background, "Title 8", "This is description 8"));
+//        productList.add(new Product(R.drawable.ic_launcher_background, "Title 9", "This is description 9"));
+//        productList.add(new Product(R.drawable.ic_launcher_background, "Title 10", "This is description 10"));
+//
+//        return productList;
+//    }
+
+    AdapterView.OnItemClickListener onItemClick = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            //Do any thing when user click to item
+            Toast.makeText(getApplicationContext(), productList.get(position).getTitle() + " - " + productList.get(position).getDescription(), Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item_menu_1:
+                if(VIEW_MODE_LISTVIEW == currentViewMode) {
+                    currentViewMode = VIEW_MODE_GRIDVIEW;
+                } else {
+                    currentViewMode = VIEW_MODE_LISTVIEW;
+                }
+                //Switch view
+                switchView();
+                //Save view mode in share reference
+                SharedPreferences sharedPreferences = getSharedPreferences("ViewMode", MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putInt("currentViewMode", currentViewMode);
+                editor.commit();
+
+                break;
+        }
+        return true;
+    }
 }
